6cfed026148e3fa864f6e245c754b989985e71e9
Roman Gulevatiy <gulevatiy.roman@gmail.com>
2026-02-14 12:50:12 +0400

    Implement User entity

---
+++ src/main/java/org/example/model/User.java
@@ -0,0 +1,14 @@
+package org.example.model;
+
+/**
+ * Created by Roman Gulevatiy on 14.02.2026.
+ * github github.com/gulevatiy-roman
+ */
+public class User {
+
+    private Long id;
+
+    private String username;
+
+    private String password;
+}
