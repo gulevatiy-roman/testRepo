bf29ec777b2c256dad1772fca38a3f59cfee28c9
Roman Gulevatiy <gulevatiy.roman@gmail.com>
2026-02-14 20:27:01 +0400

    refactor code

--- src/main/java/org/example/model/Main.java
+++ src/main/java/org/example/model/Main.java
@@ -6,6 +6,6 @@ package org.example.model;
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello World!!!");
     }
 }
